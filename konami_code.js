137afaea55921f6e8b08f71a3ceefe940d10374d
Clovis Rodríguez <clovis1992@gmail.com>
2018-01-24 21:04:11 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  
+  let bodyEvent = document.querySelector('body');
+
+  bodyEvent.addEventListener('keydown', function(e){
+    let index = 0;
+
+    console.log(e.which);
+
+    if (parseInt(e.which) === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert('Good for you!!')
+      } else {
+        index = 0;
+      }
+    }
+  })
+
 }
+
+init()
